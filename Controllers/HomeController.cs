e18985ea1cd36d5a587db23cdd7a6a2aa86b99cf
Cristhian Ebert <cristhianREbert@gmail.com>
2019-10-13 17:25:15 -0300

    CRUD Consumindo WebService

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AplicacaoCliente.Models;
+
+namespace AplicacaoCliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ClienteModel objCLiente = new ClienteModel();
+            ViewBag.listaClientes = objCLiente.ListarTodosClientes();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrar(int? id)
+        {
+            if (id != null)
+            {
+                ViewBag.Registro = new ClienteModel().Carregar(id);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(ClienteModel dados)
+        {
+            dados.Inserir();
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ViewData["ClienteID"] = id.ToString();
+            return View();
+        }
+
+        public IActionResult ExcluirCliente(int id)
+        {
+            new ClienteModel().Excluir(id);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Util/WebAPI.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Text;
+
+namespace AplicacaoCliente.Util
+{
+    public class WebAPI
+    {
+        private static string URI = "https://localhost:44352/api/cliente/";
+        private static string TOKEN = "das98d7a9sd87a98d7a9s8d7";
+
+        public static string Request_GETDELETE(string metodo, string parametro, string tipo)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URI + metodo + "/" + parametro);
+            request.Headers.Add("Token", TOKEN);
+            request.Method = tipo;
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString;
+        }
+
+        public static string RequestGET(string metodo, string parametro)
+        {
+            return Request_GETDELETE(metodo, parametro, "GET");
+        }
+
+        public static string RequestDELETE(string metodo, string parametro)
+        {
+            return Request_GETDELETE(metodo, parametro, "DELETE");
+        }
+
+        public static string RequestPOST(string metodo, string jsonData)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URI + metodo);
+            var data = Encoding.ASCII.GetBytes(jsonData);
+            request.Method = "POST";
+            request.Headers.Add("Token", TOKEN);
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public static string RequestPUT(string metodo, string jsonData)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URI + metodo);
+            var data = Encoding.ASCII.GetBytes(jsonData);
+            request.Method = "PUT";
+            request.Headers.Add("Token", TOKEN);
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+    }
+}
